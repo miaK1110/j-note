03a53a630616eb69a2390e6f817811b4d9e557cb
mia <m.09048030369@gmail.com>
2021-10-23 00:23:52 +1000

    :memo: change file

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+
+// base style
+import theme from './theme/Theme';
+import { Header } from './pages/Header';
 
 const App = () => {
-  return <h1>Laravel SPA!!</h1>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Header />
+      <h1>Laravel SPA!!</h1>;
+    </ChakraProvider>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
