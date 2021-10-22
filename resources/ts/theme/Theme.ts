4d6ed4c86e6282b6a4c0a8d5d53f0e97db5690b9
mia <m.09048030369@gmail.com>
2021-10-23 00:15:58 +1000

    :lipstick: base style

---
+++ resources/ts/theme/Theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: {},
+  },
+  colors: {
+    background: {
+      // blanched almond color
+      100: '#ffebcd',
+    },
+  },
+});
+
+export default theme;
