54b719274fb9dd67e8de8726482f87723e8c2c2a
mia <m.09048030369@gmail.com>
2021-10-27 16:11:16 +1000

    :wrench: changed webpack.mix.js file

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,11 +6,31 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .react()
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+  .ts('resources/ts/index.tsx', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css', [
+    //
+  ]);
+
+if (mix.inProduction()) {
+  // cachebuster
+  mix.version();
+}
+
+// hot reload
+mix.browserSync({
+  proxy: {
+    target: 'http://127.0.0.1:8000',
+  },
+  files: [
+    // watch these files(not working for tsx files atm)
+    'resources/views/**/*.blade.php',
+    'resources/sass/*.scss',
+    'resources/ts/*.tsx',
+  ],
+});
